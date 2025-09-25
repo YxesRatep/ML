9264c58b0fa375258517b61c13c2d9538fb0b064
Penisini Gusini <penisini@gusini.in.deinem.po>
2025-09-25 11:24:55 +0000

    pooper

--- a01/a01_4.py
+++ a01/a01_4.py
@@ -21,7 +21,7 @@ from sklearn.model_selection import KFold
 # %autoreload 2
 
 from a01_helper import *
-from a01_functions import nb_train, nb_predict
+from a01_functions import nb_train, nb_predict, nb_train_fast
 
 # %%
 # To create folds, you can use:
@@ -31,11 +31,26 @@ for i_train, i_test in Kf.split(X):
     # code here is executed K times, once per test fold
     # i_train has the row indexes of X to be used for training
     # i_test has the row indexes of X to be used for testing
+
     print(
         "Fold has {:d} training points and {:d} test points".format(
             len(i_train), len(i_test)
         )
     )
+    
+    X_i = X[i_train]
+    X_t = X[i_test]
+    y_i = y[i_train]
+    y_t = y[i_test]
+
+    model = nb_train_fast(X_i, y_i, alpha=2)
+    y_pred = nb_predict(model, X_t)
+
+    # 1-0 loss
+    loss = len(np.intersect1d(y_t, y_pred))/len(i_test)
+    print(loss)
+
+# %%
 
 # %%
 # Use cross-validation to find a good value of alpha. Also plot the obtained
