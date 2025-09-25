90f55e27a93813ea4f333b27116486e4e75a996c
Penisini Gusini <penisini@gusini.in.deinem.po>
2025-09-25 12:06:39 +0000

    04

--- a01/a01_4.py
+++ a01/a01_4.py
@@ -24,35 +24,6 @@ from a01_helper import *
 from a01_functions import nb_train, nb_predict, nb_train_fast
 
 # %%
-# To create folds, you can use:
-K = 5
-Kf = KFold(n_splits=K, shuffle=True)
-for i_train, i_test in Kf.split(X):
-    # code here is executed K times, once per test fold
-    # i_train has the row indexes of X to be used for training
-    # i_test has the row indexes of X to be used for testing
-
-    print(
-        "Fold has {:d} training points and {:d} test points".format(
-            len(i_train), len(i_test)
-        )
-    )
-    
-    X_i = X[i_train]
-    X_t = X[i_test]
-    y_i = y[i_train]
-    y_t = y[i_test]
-
-    model = nb_train_fast(X_i, y_i, alpha=2)
-    y_pred = nb_predict(model, X_t)
-
-    # 1-0 loss
-    loss = len(np.intersect1d(y_t, y_pred))/len(i_test)
-    print(loss)
-
-# %%
-
-# %%
 # Use cross-validation to find a good value of alpha. Also plot the obtained
 # accuracy estimate (estimated from CV, i.e., without touching test data) as a
 # function of alpha.
