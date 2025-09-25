9ad69fd05843f69806cfba7a51980812b2cab9b0
Petar Malamov <pmalamov@mail.uni-mannheim.de>
2025-09-25 14:46:02 +0200

    fick dich

--- a01/a01_4.py
+++ a01/a01_4.py
@@ -20,22 +20,46 @@ from sklearn.model_selection import KFold
 # %load_ext autoreload
 # %autoreload 2
 
+from tqdm import tqdm
+
 from a01_helper import *
-from a01_functions import nb_train, nb_predict
+from a01_functions import nb_train, nb_predict, nb_train_fast
 
 # %%
 # To create folds, you can use:
-K = 5
+K = 20
 Kf = KFold(n_splits=K, shuffle=True)
-for i_train, i_test in Kf.split(X):
-    # code here is executed K times, once per test fold
-    # i_train has the row indexes of X to be used for training
-    # i_test has the row indexes of X to be used for testing
-    print(
-        "Fold has {:d} training points and {:d} test points".format(
-            len(i_train), len(i_test)
+
+alpha_grid = np.linspace(2,15,60)
+
+for a in tqdm(alpha_grid, desc="Alphas"):
+    for i_train, i_test in tqdm(Kf.split(X), desc=f"Folds (alpha={a})", leave=False):
+        # code here is executed K times, once per test fold
+        # i_train has the row indexes of X to be used for training
+        # i_test has the row indexes of X to be used for testing
+    
+        print(
+            "Fold has {:d} training points and {:d} test points".format(
+                len(i_train), len(i_test)
+            )
         )
-    )
+        
+        X_i = np.take(X, i_train, axis=0)
+        y_i = np.take(y, i_train, axis=0)
+        X_t = np.take(X, i_test,  axis=0)
+        y_t = np.take(y, i_test,  axis=0)
+        
+        model = nb_train(X_i, y_i, alpha=a)
+        y_pred = nb_predict(model, X_t)['yhat']
+    
+        #pred_vec = np.equal(y_pred,y_t)
+        #false, true = np.bincount(pred_vec)
+        #tpr = true/len(pred_vec)
+        acc = np.mean(y_pred == y_t)
+        
+        print(acc)
+
+# %%
 
 # %%
 # Use cross-validation to find a good value of alpha. Also plot the obtained
--- a01/a01_functions.py
+++ a01/a01_functions.py
@@ -58,11 +58,9 @@ def nb_train(X, y, alpha=1, K=None, C=None):
     if C is None:
         C = np.max(y) + 1
 
-    actual_alpha = alpha - 1
-    
     # Compute class priors and store them in priors
     counts = np.bincount(y, minlength=C).astype(float)
-    priors = (counts + actual_alpha) / (N + C * actual_alpha)
+    priors = (counts + (alpha - 1)) / (N + C * (alpha - 1))
     
     # Compute class-conditional densities in a class x feature x value array
     # and store them in cls.
@@ -78,12 +76,48 @@ def nb_train(X, y, alpha=1, K=None, C=None):
         Nc = Xc.shape[0]
         for j in range(D):
             vcounts = np.bincount(Xc[:, j], minlength=K).astype(float)
-            cls[c, j, :] = (vcounts + actual_alpha) / (Nc + K * actual_alpha)
+            cls[c, j, :] = (vcounts + alpha - 1) / (Nc + K * (alpha - 1))
 
     return dict(
         logpriors=np.log(priors),
         logcls=np.log(cls),
     )
+
+
+# %%
+import numpy as np
+
+def nb_train_fast(X, y, C=None, K=None, alpha=1.0):
+    # X: (N, D) ints in [0, K-1]
+    # y: (N,)  ints in [0, C-1]
+    N, D = X.shape
+    if K is None:
+        K = np.max(X) + 1
+    if C is None:
+        C = np.max(y) + 1
+
+    alpha -= 1
+
+    y_onehot = np.eye(C, dtype=np.float64)[y]      # (N, C)
+    x_onehot = np.eye(K, dtype=np.float64)[X]      # (N, D, K)
+
+    # counts[c, d, k] = sum over N of 1[y_n=c]*1[X_{n,d}=k]
+    counts = np.einsum('nc,ndk->cdk', y_onehot, x_onehot, optimize=True)  # (C, D, K)
+
+    Nc = y_onehot.sum(axis=0).reshape(C, 1, 1)     # (C,1,1)
+
+    # Laplace/Dirichlet smoothing: (counts + alpha) / (Nc + K*alpha)
+    cls = (counts + alpha) / (Nc + K * alpha)
+
+    # Priors
+    priors = (Nc.squeeze() / N)
+    logpriors = np.log(np.clip(priors, 1e-300, 1.0))
+
+    return dict(
+        logpriors=np.log(priors),
+        logcls=np.log(cls),
+    )
+
 
 
 # %%
@@ -112,9 +146,9 @@ def nb_predict(model, Xnew):
     Nnew = Xnew.shape[0]
     C, D, K = logcls.shape
 
-    print(Xnew.shape)
-    print(logpriors.shape)
-    print(logcls.shape)
+    # print(f"{Xnew.shape = }")
+    # print(f"{logpriors.shape = }")
+    # print(f"{logcls.shape = }")
 
     
     # Compute the unnormalized log joint probabilities P(Y=c, x_i) of each
@@ -133,15 +167,16 @@ def nb_predict(model, Xnew):
             for d in range(D):
                 logjoint[i, c] += logcls[c, d, Xnew[i,d]]
                 
-    print(logjoint.shape)
-    print(logjoint)
-    print(np.exp(logjoint))
+    # print(logjoint.shape)
+    # print(logjoint)
+    # print(np.exp(logjoint))
     # logsumexponent := lsex 
+    
     lsex = logsumexp(logjoint.T)
-    print("lsex " + str(lsex))
+    # print(f"{lsex = }")
     
     norm_logjoint = logjoint.T - lsex
-    print(norm_logjoint)
+    # print(f"{norm_logjoint = }")
 
 
     # Compute predicted labels (in "yhat") and their log probabilities
